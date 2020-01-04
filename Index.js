722a5bbc4fd51f7654574c8d161e1871d2516d8f
urir-dev <59407773+urir-dev@users.noreply.github.com>
2020-01-04 19:43:00 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -2,6 +2,8 @@
 
 console.log("Start")
 var i = 0
+var multipl = 213 * 51
+var divid = 213 / 51
 
 document.getElementById("submit")
     .addEventListener("click", function () {
@@ -9,13 +11,12 @@ document.getElementById("submit")
   if (i == 0) {
     document.getElementById('txt').innerHTML = "Hello World!";
   } else if (i == 1 {
-    document.getElementById('txt').innerHTML = 213 * 51;
+    document.getElementById('txt').innerHTML = multipl;
   }
     else if (i == 2 {
+    document.getElementById('txt').innerHTML = divid;
+  }
+    else if (i == 3 {
     document.getElementById('txt').innerHTML = 213 / 51;
-
-            }
-
-
-            }
+  }
 
