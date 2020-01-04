50fc3554631a301132aa581c1d83350c1eadf588
urir-dev <59407773+urir-dev@users.noreply.github.com>
2020-01-04 18:52:46 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,5 +1,14 @@
 
 
+console.log("Start")
 
+
+document.getElementById("submit")
+    .addEventListener("click", function () {
+        console.log("Submit!");
+        console.log(document.getElementById("userName").value);
+        console.log(document.getElementById("password").value);
+        console.log(document.getElementById("age").value);
+    });
 
 document.getElementById('log').innerHTML = "text";
