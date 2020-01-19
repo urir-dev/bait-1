045ba2bf6b6363a453b2394875d1f26aa5db7d63
Uri <Redler.uri@gmail.com>
2020-01-19 17:57:04 +0200

    g

---
+++ JQuery2/index.js
@@ -0,0 +1,19 @@
+
+
+
+$("#mark span").css("background","yellow");
+
+$(document).ready(function(){
+    alert("Hello World!")
+});
+
+$("p").click(function (e) { 
+    $(this).slideUp
+    
+});
+
+$("p.normal").hide();
+
+$("button").click(function(){
+
+})
